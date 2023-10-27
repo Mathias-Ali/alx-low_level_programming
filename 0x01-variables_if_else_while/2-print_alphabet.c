49845a5272773268dc40e70a4e8a06bf91ad7beb
Mathias <alimathiasdenen@gmail.com>
2023-10-27 07:11:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = a; a <= z; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
